6a3ca9b06cab0549816ac60f687f67588b658d7c
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 21:00:11 -0400

    Creating intials migrations.

---
+++ GatewaysManagement/GatewaysManagement.Data/Migrations/20221007170641_Initial.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GatewaysManagement.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gateways",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SerialNumber = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IPAdress = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gateways", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UID = table.Column<int>(type: "int", nullable: false),
+                    Vendor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    GatewayId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_Gateways_GatewayId",
+                        column: x => x.GatewayId,
+                        principalTable: "Gateways",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_GatewayId",
+                table: "Devices",
+                column: "GatewayId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_UID",
+                table: "Devices",
+                column: "UID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gateways_SerialNumber",
+                table: "Gateways",
+                column: "SerialNumber",
+                unique: true,
+                filter: "[SerialNumber] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Gateways");
+        }
+    }
+}
