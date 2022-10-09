6167a34c8835feacedd28eea715600308e5d2bb3
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 20:53:59 -0400

    Creating Data Annotation to validate IPv4 Adress.

---
+++ GatewaysManagement/GatewaysManagement.Common/DTO/Validations/IPAdressAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace GatewaysManagement.Common.DTO.Validations
+{
+    class IPAdressAttribute : ValidationAttribute
+    {
+        public int Minimum { get; set; }
+        public int Maximum { get; set; }
+
+        public IPAdressAttribute()
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            string strValue = value as string;
+            if (!string.IsNullOrEmpty(strValue))
+            {
+                if (strValue.Count(c => c == '.') != 3) return false;
+
+                return IPAddress.TryParse(strValue, out IPAddress address);
+            }
+            return true;
+        }
+    }
+}
