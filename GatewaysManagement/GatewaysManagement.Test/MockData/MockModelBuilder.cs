60f2c6febbece0b8d6a18b71a7f5e037a92cdb0c
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 21:16:14 -0400

    Creating Mock clasess.

---
+++ GatewaysManagement/GatewaysManagement.Test/MockData/MockModelBuilder.cs
@@ -0,0 +1,29 @@
+﻿using GatewaysManagement.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GatewaysManagement.Test.MockData
+{
+    public class MockModelBuilder
+    {
+        public GatewayFactory GatewayPrototype { get; set; }
+        public DeviceFactory DevicePrototype { get; set; }
+
+        public MockModelBuilder()
+        {
+            GatewayPrototype = new GatewayFactory();
+            DevicePrototype = new DeviceFactory();
+        }
+
+        public Gateway GetMockGateway(Guid gatewayId)
+        {
+            return GatewayPrototype.GetSingleObject(gatewayId);
+        }
+
+        public Device GetMockDevice(Guid deviceId)
+        {
+            return DevicePrototype.GetSingleObject(deviceId);
+        }
+    }
+}
---
+++ GatewaysManagement/GatewaysManagement.Test/MockData/MockModelFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GatewaysManagement.Test.MockData
+{
+    public abstract class MockModelFactory<T>
+    {
+        public abstract T GetSingleObject(Guid id);
+    }
+}
