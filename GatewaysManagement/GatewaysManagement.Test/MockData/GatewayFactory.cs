3b5e67208ca2401fe21aac39b46cecc87fb2c5a0
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 21:16:43 -0400

    Creating Gateway Factory.

---
+++ GatewaysManagement/GatewaysManagement.Test/MockData/GatewayFactory.cs
@@ -0,0 +1,21 @@
+﻿using GatewaysManagement.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GatewaysManagement.Test.MockData
+{
+    public class GatewayFactory : MockModelFactory<Gateway>
+    {
+        public override Gateway GetSingleObject(Guid id)
+        {            
+            return new Gateway
+            {
+                Id = id,
+                Name = "Test Name",
+                IPAdress = "192.168.1.1",
+                SerialNumber = "123456789"
+            };
+        }
+    }
+}
