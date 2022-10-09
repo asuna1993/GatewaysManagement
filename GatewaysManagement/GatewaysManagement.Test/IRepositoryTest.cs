917849d3d5edecee196740f612ff80e58a7d717d
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 21:15:39 -0400

    Creating interfaces for testing clasess.

---
+++ GatewaysManagement/GatewaysManagement.Test/IRepositoryTest.cs
@@ -0,0 +1,12 @@
+﻿using GatewaysManagement.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GatewaysManagement.Test
+{
+    public interface IRepositoryTest
+    {
+        CoreDbContext GetSampleData(string db);
+    }
+}
---
+++ GatewaysManagement/GatewaysManagement.Test/IServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using GatewaysManagement.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GatewaysManagement.Test
+{
+    public interface IServiceTest<T>
+    {
+        CoreDbContext GetSampleData(string db);
+
+        T InitService(CoreDbContext context);
+    }
+}
