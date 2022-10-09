498f8129a34ae0294414060d54a06925da0dd131
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 20:43:16 -0400

    Creating DbContext.

---
+++ GatewaysManagement/GatewaysManagement.Data/CoreDbContext.cs
@@ -0,0 +1,28 @@
+﻿using GatewaysManagement.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GatewaysManagement.Data
+{
+    public class CoreDbContext : DbContext
+    {
+        public DbSet<Gateway> Gateways { get; set; }
+        public DbSet<Device> Devices { get; set; }
+
+        public CoreDbContext(DbContextOptions<CoreDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Gateway>()
+                   .HasIndex(d => d.SerialNumber)
+                   .IsUnique();
+
+            builder.Entity<Device>()
+                   .HasIndex(d => d.UID)
+                   .IsUnique();
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
