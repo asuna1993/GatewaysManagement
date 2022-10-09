225e6140beb8dd6732248488f75327c7413346c1
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 20:46:33 -0400

    Creating repositories.

---
+++ GatewaysManagement/GatewaysManagement.Data/Repository/DeviceRepository.cs
@@ -0,0 +1,21 @@
+﻿using GatewaysManagement.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GatewaysManagement.Data.Repository
+{
+    public class DeviceRepository : GenericRepository<Device>, IDeviceRepository
+    {
+        public DeviceRepository(CoreDbContext context)
+            : base(context)
+        {
+        }
+
+        public bool Exists(Guid deviceId)
+        {
+            return _context.Devices.Any(b => b.Id == deviceId);
+        }
+    }
+}
---
+++ GatewaysManagement/GatewaysManagement.Data/Repository/GatewayRepository.cs
@@ -0,0 +1,22 @@
+﻿using GatewaysManagement.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GatewaysManagement.Data.Repository
+{
+    public class GatewayRepository: GenericRepository<Gateway>, IGatewayRepository
+    {
+        public GatewayRepository(CoreDbContext context)
+            : base(context)
+        {
+        }
+
+        public bool Exists(Guid gatewayId)
+        {
+            return _context.Gateways.Any(b => b.Id == gatewayId);
+        }
+
+    }
+}
