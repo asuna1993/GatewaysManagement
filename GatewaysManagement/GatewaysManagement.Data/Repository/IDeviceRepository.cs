107e8c2e1be856fdce133da23ed79b819ce804d1
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 20:44:48 -0400

    Creating interfaces for repositories.

---
+++ GatewaysManagement/GatewaysManagement.Data/Repository/IDeviceRepository.cs
@@ -0,0 +1,12 @@
+﻿using GatewaysManagement.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GatewaysManagement.Data.Repository
+{
+    public interface IDeviceRepository : IGenericRepository<Device>
+    {
+        bool Exists(Guid deviceId);
+    }
+}
---
+++ GatewaysManagement/GatewaysManagement.Data/Repository/IGatewayRepository.cs
@@ -0,0 +1,12 @@
+﻿using GatewaysManagement.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GatewaysManagement.Data.Repository
+{
+    public interface IGatewayRepository : IGenericRepository<Gateway>
+    {
+        bool Exists(Guid gatewayId);
+    }
+}
