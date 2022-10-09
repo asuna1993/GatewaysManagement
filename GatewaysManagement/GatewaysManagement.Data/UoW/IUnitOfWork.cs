3c596c1cf8bbb9f914def1a38f56ea9e2cad5241
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 20:43:51 -0400

    Implementing UnitOfWork.

---
+++ GatewaysManagement/GatewaysManagement.Data/UoW/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using GatewaysManagement.Data.Entities;
+using GatewaysManagement.Data.Repository;
+using System;
+using System.Threading.Tasks;
+
+namespace GatewaysManagement.Data.UoW
+{
+    public interface IUnitOfWork
+    {
+        IGatewayRepository GatewayRepository { get; set; }
+        IDeviceRepository DeviceRepository { get; set; }
+        Task<int> CommitAsync();
+    }
+}
---
+++ GatewaysManagement/GatewaysManagement.Data/UoW/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using GatewaysManagement.Data.Entities;
+using GatewaysManagement.Data.Repository;
+using System;
+using System.Threading.Tasks;
+
+namespace GatewaysManagement.Data.UoW
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly CoreDbContext _context;
+
+        public UnitOfWork(CoreDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            GatewayRepository ??= new GatewayRepository(_context);
+            DeviceRepository ??= new DeviceRepository(_context);
+        }
+
+        public IGatewayRepository GatewayRepository { get; set; }
+        public IDeviceRepository DeviceRepository { get; set; }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
