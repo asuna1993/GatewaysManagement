fea89e360b5dde18ff71beaaff01ecb334ce0cb5
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-08 21:14:14 -0400

    Creating Gateway Service Interface.

---
+++ GatewaysManagement/GatewaysManagement.Services/IGatewayService.cs
@@ -0,0 +1,20 @@
+﻿using GatewaysManagement.Data.Entities;
+using GatewaysManagement.Services.Utils;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GatewaysManagement.Services
+{
+    public interface IGatewayService
+    {
+        bool GatewayExists(Guid gatewayId);
+        Task<Gateway> GetGatewayAsync(Guid gatewayId);
+        Task<IEnumerable<Gateway>> GetGatewaysAsync();
+        Task<PageList<Gateway>> GetGatewaysAsync(ResourceParameters resourceParameters);
+        Task AddGatewayAsync(Gateway gateway);
+        Task UpdateGatewayAsync(Guid gatewayId, Gateway gatewayToBeUpdate);
+        Task DeleteGatewayAsync(Gateway gateway);
+
+    }
+}
