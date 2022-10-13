90d8c96149368b10baf605a313f929077638a943
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-13 17:50:47 -0400

    Configuring start up.

---
+++ GatewaysManagement/GatewaysManagement.API/ServicesExtensions.cs
@@ -0,0 +1,173 @@
+﻿using GatewaysManagement.API.Utils.JsonLocalization;
+using GatewaysManagement.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace GatewaysManagement.API
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContextPool<CoreDbContext>(
+                dbContextOptions => dbContextOptions
+                    .UseSqlServer(
+                        // Replace with your connection string.
+                        config.GetConnectionString("ApiConnection"),
+                        x => x.MigrationsAssembly("GatewaysManagement.Data")
+                     //Consider to use a specific charset according with your needs in your modelbuilder inside your context
+                     )
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors()
+            );
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                // define swagger docs and other options
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API Gateways Management",
+                    Version = "v1",
+                    Description = "REST service for storing information about the gateways and their associated devices."
+                });
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter JWT Bearer authorization token",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer", // must be lowercase!!!
+                    BearerFormat = "Bearer {token}",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        // defines scope - without a protocol use an empty array for global scope
+                        { securityScheme, Array.Empty<string>() }
+                    }
+                );
+            });
+        }
+
+        public static void ConfigureTokenAuth(this IServiceCollection services, IConfiguration config)
+        {
+            var key = Encoding.UTF8.GetBytes(config.GetSection("BearerTokens")["Key"]);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidAudience = config.GetSection("BearerTokens")["Audience"],
+                    ValidateAudience = true,
+                    ValidIssuer = config.GetSection("BearerTokens")["Issuer"],
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateLifetime = true
+                };
+            });
+        }
+
+        public static void ConfigurePerformance(this IServiceCollection services)
+        {
+            services.AddMemoryCache();
+
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "image/svg+xml" });
+            });
+
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            });
+        }
+
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+           {
+               options.AddDefaultPolicy(builder =>
+                   builder.SetIsOriginAllowed(_ => true)
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowCredentials()
+                .WithExposedHeaders(new string[] { "X-Pagination" }));
+           });
+        }
+
+        public static void ConfigureI18N(this IServiceCollection services)
+        {
+            services.AddLocalization(o =>
+            {
+                o.ResourcesPath = "i18n";
+            });
+            services.AddSingleton<IStringLocalizerFactory, JsonStringLocalizerFactory>();
+            services.AddSingleton<IStringLocalizer, JsonStringLocalizer>();
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix,
+                opts => { opts.ResourcesPath = "i18n"; })
+            .AddDataAnnotationsLocalization(options =>
+            {
+            });
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+        }
+
+
+        public static void ConfigureDetection(this IServiceCollection services)
+        {
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+        }
+
+        public static void ConfigureHsts(this IServiceCollection services)
+        {
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(365);
+            });
+        }
+    }
+}
---
+++ GatewaysManagement/GatewaysManagement.API/Startup.cs
@@ -0,0 +1,147 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using GatewaysManagement.API.Filters;
+using GatewaysManagement.API;
+using GatewaysManagement.Data.UoW;
+using GatewaysManagement.Data.Repository;
+using Newtonsoft.Json;
+using GatewaysManagement.Services;
+using GatewaysManagement.Services.Impls;
+using GatewaysManagement.Services.Utils;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GatewaysManagement
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureI18N();
+            services.ConfigureCors();
+            services.AddMvc(config =>
+            {
+                config.Filters.Add(typeof(ApiValidationFilterAttribute));
+                config.EnableEndpointRouting = false;
+            }).AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+            services.ConfigureHsts();
+            services.AddControllers(setupAction => {}).AddNewtonsoftJson(setupAction =>
+           {
+               setupAction.SerializerSettings.ContractResolver =
+                  new CamelCasePropertyNamesContractResolver();
+           }).ConfigureApiBehaviorOptions(setupAction =>
+           {
+               setupAction.InvalidModelStateResponseFactory = context =>
+               {
+                    // create a problem details object
+                    var problemDetailsFactory = context.HttpContext.RequestServices
+                       .GetRequiredService<ProblemDetailsFactory>();
+                   var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                           context.HttpContext,
+                           context.ModelState);
+
+                    // add additional info not added by default
+                    problemDetails.Detail = "See the errors field for details.";
+                   problemDetails.Instance = context.HttpContext.Request.Path;
+
+                    // find out which status code to use
+                    var actionExecutingContext =
+                         context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                    // if there are modelstate errors & all keys were correctly
+                    // found/parsed we're dealing with validation errors
+                    if ((context.ModelState.ErrorCount > 0) &&
+                       (actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+                   {
+                       problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                       problemDetails.Title = "One or more validation errors occurred.";
+
+                       return new UnprocessableEntityObjectResult(problemDetails)
+                       {
+                           ContentTypes = { "application/problem+json" }
+                       };
+                   }
+
+                    // if one of the keys wasn't correctly found / couldn't be parsed
+                    // we're dealing with null/unparsable input
+                    problemDetails.Status = StatusCodes.Status400BadRequest;
+                   problemDetails.Title = "One or more errors on input occurred.";
+                   return new BadRequestObjectResult(problemDetails)
+                   {
+                       ContentTypes = { "application/problem+json" }
+                   };
+               };
+           });
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            services.ConfigureDbContext(Configuration);
+            services.ConfigureSwagger();
+            services.ConfigureTokenAuth(Configuration);
+            services.ConfigurePerformance();
+
+            services.ConfigureDetection();
+
+            services.AddHttpContextAccessor();
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<IGatewayService, GatewayService>();
+            services.AddTransient<IDeviceService, DeviceService>();
+
+            services.AddTransient<IPropertyMappingService, PropertyMappingService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHttpsRedirection();
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                    });
+                });
+            }
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;//"explorer";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GatewaysManagement V1");
+            });
+        }
+    }
+}
