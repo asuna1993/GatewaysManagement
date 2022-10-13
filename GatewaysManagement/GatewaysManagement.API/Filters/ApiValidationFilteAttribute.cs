0b6c5b57afde4985713bca9e285c3f35cd96e2c0
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-13 17:48:50 -0400

    Creating Filter class.

---
+++ GatewaysManagement/GatewaysManagement.API/Filters/ApiValidationFilteAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace GatewaysManagement.API.Filters
+{
+    public class ApiValidationFilterAttribute : ActionFilterAttribute
+
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
