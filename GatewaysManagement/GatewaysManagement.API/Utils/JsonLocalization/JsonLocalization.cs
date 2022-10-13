6335ff7ba542287533e81678d0b76320f4ee8599
Betty Gutierrez <betty.gutierrez@ntsprint.com>
2022-10-13 17:48:20 -0400

    Creating Util classes for Localization.

---
+++ GatewaysManagement/GatewaysManagement.API/Utils/JsonLocalization/JsonLocalization.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GatewaysManagement.API.Utils.JsonLocalization
+{
+    public class JsonLocalization
+    {
+        public string Key { get; set; }
+
+        public Dictionary<string, string> LocalizedValue = new Dictionary<string, string>();
+    }
+}
